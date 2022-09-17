f0a98a7900f15645ddf8a0c800d78aab70268847
Nut Pinyo <bomb.np@gmail.com>
2022-09-17 15:32:56 +0700

    init: terraform project

---
+++ variables.tf
@@ -0,0 +1,58 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "availability_zone" {
+  description = "AWS availability zone"
+  type        = string
+  default     = "us-west-2ax"
+}
+
+variable "ami" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket for this project"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "database_name" {
+  description = "Name of the database"
+  type        = string
+}
+
+variable "database_user" {
+  description = "Name of the database user"
+  type        = string
+}
+
+variable "database_pass" {
+  description = "Password for the database user"
+  type        = string
+}
+
+variable "database_port" {
+  description = "Port for the database"
+  type        = number
+  default     = 3306
+}
+
+variable "admin_user" {
+  description = "Name of the admin user"
+  type        = string
+}
+
+variable "admin_pass" {
+  description = "Password for the admin user"
+  type        = string
+}
