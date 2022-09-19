4165bf6c85fff6d351a927c5fdf07738c55dfeb3
Nut Pinyo <bomb.np@gmail.com>
2022-09-19 10:48:02 +0700

    init db with script

--- main.tf
+++ main.tf
@@ -13,11 +13,11 @@ provider "aws" {
   region = var.region
 }
 
-module "vpc" {
-  source = "./modules/vpc"
+# module "vpc" {
+#   source = "./modules/vpc"
 
-  availability_zone = var.availability_zone
-}
+#   availability_zone = var.availability_zone
+# }
 
 module "ec2_instances" {
   source = "./modules/ec2_instances"
@@ -25,4 +25,9 @@ module "ec2_instances" {
   ami               = var.ami
   availability_zone = var.availability_zone
   instance_type     = var.instance_type
+}
+
+# TODO: temporary, remove this later
+output "db_server_public_ip" {
+  value = module.ec2_instances.db_server_public_ip
 }
--- modules/ec2_instances/main.tf
+++ modules/ec2_instances/main.tf
@@ -1,18 +1,27 @@
-resource "aws_instance" "app_server" {
+# resource "aws_instance" "app_server" {
+#   ami               = var.ami
+#   instance_type     = var.instance_type
+#   availability_zone = var.availability_zone
+
+#   tags = {
+#     Name = "App Server"
+#   }
+# }
+
+resource "aws_instance" "db_server" {
   ami               = var.ami
   instance_type     = var.instance_type
   availability_zone = var.availability_zone
 
+  # TODO: remove this, default vpc
+  security_groups = ["launch-wizard-6"]
+  user_data       = file("init-db.sh")
+
   tags = {
-    Name = "App Server"
+    Name = "DB Server"
   }
 }
 
-# resource "aws_instance" "db_server" {
-#   ami               = var.ami
-#   instance_type     = var.instance_type
-#   availability_zone = var.availability_zone
-#   tags = {
-#     Name = "DB Server"
-#   }
-# }
+output "db_server_public_ip" {
+  value = aws_instance.db_server.public_ip
+}
